87695f9dc8cd14bbb8c96ef152492614e70840c0
Johan Sanchez <you@example.com>
2024-08-09 00:58:42 -0500

    agregeu una nueva funcion en tratamient y lo invoque main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 import exploracion as ex
-
+import exploracion as pr
 print("mi modificacion en exploracion")
+print("mi modificacion de trataemito 02")
--- tratamiento.py
+++ tratamiento.py
@@ -1,2 +1,4 @@
 def funcion_eliminando_vacios():
     return "elimine vacios"
+def funcion_tratamiento_01():
+    return "elimine2"
